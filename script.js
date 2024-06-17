4451d06f33e7614d79dfb335f63219bece6eb23a
Luiz felipe de sa ferreira <111285128+luizfelipedesa@users.noreply.github.com>
2024-06-17 11:19:35 -0300

    eu

--- script.js
+++ script.js
@@ -43,11 +43,11 @@ const perguntas = [
 ]
 
 let atual = 0;
-
 let perguntaAtual;
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 
 mostraPergunta()
